ff2543fef948b74723cb69deab20c145a98e3556
Joanne Dyer <joannecdyer@googlemail.com>
2013-01-22 16:54:25 +0000

    Changes made to player update method to create smoother movement.

--- CircleGame/Player.m
+++ CircleGame/Player.m
@@ -70,42 +70,51 @@
             [self.nextLight almostOccupyLight];
         }
     } else {
+        //either difference in height will be equal or difference in width.
         CGFloat differenceInHeight = self.nextLight.position.y - self.position.y;
         CGFloat differenceInWidth = self.nextLight.position.x - self.position.x;
         
         float distanceTravelled = SPEED_IN_POINTS_PER_SECOND * dt;
-        if (differenceInHeight == 0) {
-            if ((differenceInWidth > 0 && distanceTravelled > differenceInWidth) || (differenceInWidth < 0 && distanceTravelled < differenceInWidth)) {
-                //next light has been reached.
-                self.currentLight = self.nextLight;
-                self.nextLight = nil;
-                
-                //TODO use value.
-                [self.currentLight occupyLightAndGetValue];
-                self.position = self.currentLight.position;
-            } else if (differenceInWidth > 0) {
-                CGPoint newPosition = ccp(self.position.x + SPEED_IN_POINTS_PER_SECOND * dt, self.position.y);
-                self.position = newPosition;
+        if ((differenceInWidth > 0 && distanceTravelled > differenceInWidth) || (differenceInWidth < 0 && distanceTravelled > -differenceInWidth) ||  (differenceInHeight > 0 && distanceTravelled > differenceInHeight) || (differenceInHeight < 0 && distanceTravelled > -differenceInHeight)) {
+            //next light has been reached.
+            self.currentLight = self.nextLight;
+            //[self.route removeFirstLightFromRoute];
+            //TODO use value.
+            [self.currentLight occupyLightAndGetValue];
+            self.position = self.currentLight.position;
+            self.nextLight = [self.route getNextLightFromRoute];
+            if (self.nextLight) {
+                [self.route removeFirstLightFromRoute];
+                [self.currentLight leaveLight];
+                [self.nextLight almostOccupyLight];
+                if (differenceInWidth > 0) {
+                    distanceTravelled -= differenceInWidth;
+                } else if (differenceInWidth < 0) {
+                    distanceTravelled += differenceInWidth;
+                } else if (differenceInHeight > 0) {
+                    distanceTravelled -= differenceInHeight;
+                } else if (differenceInHeight < 0) {
+                    distanceTravelled += differenceInHeight;
+                }
+                differenceInHeight = self.nextLight.position.y - self.position.y;
+                differenceInWidth = self.nextLight.position.x - self.position.x;
             } else {
-                CGPoint newPosition = ccp(self.position.x - SPEED_IN_POINTS_PER_SECOND * dt, self.position.y);
-                self.position = newPosition;
+                distanceTravelled = 0;
             }
-        } else {
-            if ((differenceInHeight > 0 && distanceTravelled > differenceInHeight) || (differenceInHeight < 0 && distanceTravelled < differenceInHeight)) {
-                //next light has been reached.
-                self.currentLight = self.nextLight;
-                self.nextLight = nil;
-                
-                //TODO use value
-                [self.currentLight occupyLightAndGetValue];
-                self.position = self.currentLight.position;
-            } else if (differenceInHeight > 0) {
-                CGPoint newPosition = ccp(self.position.x, self.position.y + SPEED_IN_POINTS_PER_SECOND * dt);
-                self.position = newPosition;
-            } else {
-                CGPoint newPosition = ccp(self.position.x, self.position.y - SPEED_IN_POINTS_PER_SECOND * dt);
-                self.position = newPosition;
-            }
+        }
+        
+        if (differenceInWidth > 0) {
+            CGPoint newPosition = ccp(self.position.x + distanceTravelled, self.position.y);
+            self.position = newPosition;
+        } else if (differenceInWidth < 0) {
+            CGPoint newPosition = ccp(self.position.x - distanceTravelled, self.position.y);
+            self.position = newPosition;
+        } else if (differenceInHeight > 0) {
+            CGPoint newPosition = ccp(self.position.x, self.position.y + distanceTravelled);
+            self.position = newPosition;
+        } else if (differenceInHeight < 0) {
+            CGPoint newPosition = ccp(self.position.x, self.position.y - distanceTravelled);
+            self.position = newPosition;
         }
     }
 }
